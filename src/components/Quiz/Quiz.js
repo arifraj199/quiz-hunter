ccfc4964b5aeb12fad2e967db859ef1f1e6fb2d4
arifraj199 <karif9514@gmail.com>
2023-01-22 01:57:22 +0600

    update data error half solved

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import QuizQuestions from "../QuizQuestions/QuizQuestions";
 
@@ -6,19 +7,27 @@ const Quiz = () => {
   const quizLoader = useLoaderData();
   // console.log(quiz.data.questions);
   const quizData = quizLoader.data.questions;
-  console.log(quizData);
+  // console.log(quizData);
+  const [data, setData] = useState(0);
+
+  console.log(data);
   return (
     <div className="grid grid-cols-3  gap-4 mt-8">
       <div className="grid col-span-2">
         <h2 className="text-2xl font-bold text-orange-700">Questions</h2>
 
         {quizData.map((quiz) => (
-          <QuizQuestions key={quiz.id} quiz={quiz}></QuizQuestions>
+          <QuizQuestions
+            setData={setData}
+            key={quiz.id}
+            quiz={quiz}
+          ></QuizQuestions>
         ))}
       </div>
       <div>
         <h2 className="text-2xl font-bold text-blue-800">
           Questions Correction
+          <p>Right Answer:{data}</p>
         </h2>
       </div>
     </div>
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
-const QuizQuestions = ({ quiz }) => {
+const QuizQuestions = ({ quiz, setData }) => {
   const { question, correctAnswer, id, options } = quiz;
 
   const [selectAnswer, setSelectAnswer] = useState("");
+
+  const [correction, setCorrection] = useState(0);
+  const [wrongAnswer, setWrongAnswer] = useState(0);
+
   const handleQuesOption = (event) => {
     const selectOption = event.target.value;
     setSelectAnswer(selectOption);
@@ -12,9 +16,13 @@ const QuizQuestions = ({ quiz }) => {
   const handleJustify = () => {
     if (selectAnswer === correctAnswer) {
       alert("Correct Answer!!");
+      setCorrection(correction + 1);
     } else {
       alert("Wrong Answer!!");
+      setWrongAnswer(correction + 1);
     }
+
+    setData(correction + 1);
   };
   return (
     <div>
@@ -44,6 +52,10 @@ const QuizQuestions = ({ quiz }) => {
           >
             Justify Answer
           </button>
+        </div>
+        <div>
+          <p>Right Answer:{correction}</p>
+          <p>Wrong Answer:{wrongAnswer}</p>
         </div>
       </div>
     </div>
