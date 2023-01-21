5e1c6d3ad3ea20c7b6fa73076c9b09cd5166c24b
arifraj199 <karif9514@gmail.com>
2023-01-21 20:39:17 +0600

    questions functionality added

--- src/components/QuizQuesOptions/QuizQuesOptions.js
+++ src/components/QuizQuesOptions/QuizQuesOptions.js
@@ -1,29 +1,35 @@
 import React from "react";
 import { useState } from "react";
 
-const QuizQuesOptions = ({ option, correctAnswer }) => {
-    const [selectAnswer,setSelectAnswer] = useState('')
-//   console.log(correctAnswer);
- const handleQuesOption = event =>{
-    event.preventDefault();
+const QuizQuesOptions = ({ option }) => {
+  const [selectAnswer, setSelectAnswer] = useState("");
+  // console.log(correctAnswer);
+  const handleQuesOption = (event) => {
+    
     const selectOption = event.target.value;
     // console.log(selectOption);
     setSelectAnswer(selectOption);
- }
-//  if(selectAnswer === option){
-//     alert('answer is correct!');
-//  }else{
-//     alert('wrong answer');
-//  }
+    
+  };
+  
   return (
-    <div className="flex justify-center items-center">
-        <div onClick={handleQuesOption}>
-        <input  type="radio" name="option" value={option} />
-        </div>
+    <div>
+      <div className="flex justify-center items-center">
+      <div>
+        <input
+          onChange={handleQuesOption}
+          type="radio"
+          name="quesOption"
+          value={option}
+        />
+        <label className="ml-2">{option}</label>
+      </div>
       
-      <h1 className="ml-2">{option}</h1>
+    </div>
+    
     </div>
   );
 };
 
 export default QuizQuesOptions;
+
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,18 +1,49 @@
-import React from "react";
-import QuizQuesOptions from "../QuizQuesOptions/QuizQuesOptions";
+import React, { useState } from "react";
 
 const QuizQuestions = ({ quiz }) => {
-  // console.log(quiz);
   const { question, correctAnswer, id, options } = quiz;
+
+  const [selectAnswer, setSelectAnswer] = useState("");
+  const handleQuesOption = (event) => {
+    const selectOption = event.target.value;
+    setSelectAnswer(selectOption);
+  };
+
+  const handleJustify = () => {
+    if (selectAnswer === correctAnswer) {
+      alert("Correct Answer!!");
+    } else {
+      alert("Wrong Answer!!");
+    }
+  };
   return (
     <div>
       <div className="border-2  border-orange-500 rounded-md my-5 p-8 mx-4">
         <h1 className="text-blue-800 font-semibold text-xl">{question}</h1>
         <div className="grid grid-cols-2 mt-4">
-          {options.map((option, idx) => (
-            <QuizQuesOptions key={idx} correctAnswer={correctAnswer} option={option}></QuizQuesOptions>
+          {options.map((option) => (
+            <div key={id}>
+              <div className="flex justify-center items-center">
+                <div>
+                  <input
+                    onChange={handleQuesOption}
+                    type="radio"
+                    name="quesOption"
+                    value={option}
+                  />
+                  <label className="ml-2">{option}</label>
+                </div>
+              </div>
+            </div>
           ))}
-          
+        </div>
+        <div className="">
+          <button
+            onClick={handleJustify}
+            className="bg-orange-800 text-white py-1 px-6 rounded-md font-semibold mt-4"
+          >
+            Justify Answer
+          </button>
         </div>
       </div>
     </div>
